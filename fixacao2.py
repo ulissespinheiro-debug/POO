3e54e84d511ee5c015fab91abb0f693546c5ec86
ulissespinheiro-debug <ulisses.pinheiro@escolar.ifrn.edu.br>
2026-09-09 13:16:05 +0000

    feito até a 4, preguiça de fazer a 5

--- fixacao2.py
+++ fixacao2.py
@@ -23,3 +23,18 @@ p2 = point(320,160)
 circulo2 = circle(p2, 50)
 
 print(point_in_circle(circulo1, circulo2))
+
+def rect_in_circle(circle, vertices):
+    for v in vertices:
+        dx = v.x - circle.center.x
+        dy = v.y - circle.center.y
+        distancia = math.sqrt(dx**2 + dy**2)
+        if distancia > circle.radius:
+            return False
+    return False
+
+c = circle(point(0,0), 10)
+
+rect = [point(2,2), point(2,-2), point(-2,2), point(-2,-2)]
+
+print(rect_in_circle(c, rect))
