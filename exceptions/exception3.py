6ba4a290863170d769f1743a9bb51036d45d7115
ulissespinheiro-debug <ulisses.pinheiro@escolar.ifrn.edu.br>
2026-09-24 00:54:29 +0000

    Questão 3 do estacionamento commitada

--- exceptions/exception3.py
+++ exceptions/exception3.py
@@ -1,3 +1,24 @@
+from rich.traceback import install
+install(show_locals=True)
+
+class EstacionamentoLotadoError(Exception):
+    """
+    Trata do erro qaundo o estacionamento está cheio.
+    
+    """
+
 class Estacionamento:
-    def __init__(self, ):
-        pass
+    def __init__(self, vagas: int):
+        self.vagas = vagas
+
+    def entrar(self, carros):
+        if carros > self.vagas:
+            raise EstacionamentoLotadoError(
+                f"O número de vagas já foi excedida!"
+            )
+        self.vagas -= carros
+        return carros
+
+estacio1 = Estacionamento(30)
+print(f"Após entrarem {estacio1.entrar(31)} carros, sobraram {estacio1.vagas} vagas")
+                
